caf392bac88fd4b20687c8deb098e2616fbd5a0d
Holymaiden <haikalwatampone3@gmail.com>
2023-08-08 22:35:58 +0800

    Fix Bug Sign In

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -127,7 +127,7 @@
             </div>
             <!--end::Aside-->
             <!--begin::Body-->
-            <div class="d-none d-lg-flex flex-lg-row-fluid w-50 bgi-size-cover bgi-position-y-center bgi-position-x-start bgi-no-repeat" style="background-image: url({{ asset('assets/media/auth/bg11.png') }})"></div>
+            <div class="d-none d-lg-flex flex-lg-row-fluid w-50 bgi-size-cover bgi-position-y-center bgi-position-x-start bgi-no-repeat" style="background-image: url(assets/media/auth/bg11.png)"></div>
             <!--begin::Body-->
         </div>
         <!--end::Authentication - Sign-in-->
