bda00c21c7265c93efee535c67bddffe66c968c6
Holymaiden <haikalwatampone3@gmail.com>
2023-08-28 19:15:30 +0800

    fix: ico

--- resources/views/_layouts/sidebar.blade.php
+++ resources/views/_layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
     <div class="app-sidebar-logo px-6" id="kt_app_sidebar_logo">
         <!--begin::Logo image-->
         <a href="{{ route('dashboard') }}">
-            <img alt="Logo" src="{{ asset('assets/media/logos/default-dark.svg') }}" class="h-25px app-sidebar-logo-default" />
+            <img alt="Logo" src="{{ asset('assets/media/logos/default-dark.png') }}" class="h-25px app-sidebar-logo-default" />
             <img alt="Logo" src="{{ asset('assets/media/logos/default-small.svg') }}" class="h-20px app-sidebar-logo-minimize" />
         </a>
         <!--end::Logo image-->
