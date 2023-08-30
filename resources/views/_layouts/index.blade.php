4367aa9bdf1859c2bf7e93695ddfab9e8266be9d
Holymaiden <haikalwatampone3@gmail.com>
2023-08-30 21:47:37 +0800

    refactor: ico

--- resources/views/_layouts/index.blade.php
+++ resources/views/_layouts/index.blade.php
@@ -10,7 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:site_name" content="Derinos Group" />
         <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
-        <link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+        <link rel="shortcut icon" href="{{ asset('assets/media/logos/favicon.ico') }}" />
         <!--begin::Fonts-->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
         <!--end::Fonts-->
